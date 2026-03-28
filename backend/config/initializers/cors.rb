bef1307c40ebc8c12f765ed5481a1fec36d67920
Kaitlyn Swann <kaitlyn@hey.com>
2026-03-28 17:53:30 -0600

    attempt to resolve github actions failures

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -6,10 +6,10 @@
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins ENV["FRONTEND_URL"]
-      resource "*",
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
+  allow do
+    origins ENV.fetch("FRONTEND_URL", "http://localhost:5173")
+    resource "*",
+      headers: :any,
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
   end
+end
